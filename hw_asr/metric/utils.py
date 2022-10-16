05896dedddd930d7ce9eb6c85498dcecf84387c6
vslvskyy <75453192+vslvskyy@users.noreply.github.com>
2022-10-16 20:15:07 +0300

    lower() in calc_ser/calc_wer

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -5,13 +5,13 @@ import editdistance
 
 def calc_cer(target_text, predicted_text) -> float:
     if target_text:
-        return editdistance.distance(target_text, predicted_text) / len(target_text)
+        return editdistance.distance(target_text.lower(), predicted_text.lower()) / len(target_text)
     return 1.0
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    target_words = target_text.split()
-    predicted_words = predicted_text.split()
+    target_words = target_text.lower().split()
+    predicted_words = predicted_text.lower().split()
     if target_words:
         return editdistance.distance(target_words, predicted_words) / len(target_words)
     return 1.0
