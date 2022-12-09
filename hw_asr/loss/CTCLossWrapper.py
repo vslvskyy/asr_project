c7fa45dbcc9b5236b79a52a9f58de6fa23ed2fb7
vslvskyy <75453192+vslvskyy@users.noreply.github.com>
2022-12-09 18:27:34 +0300

    Delete prints

--- hw_asr/loss/CTCLossWrapper.py
+++ hw_asr/loss/CTCLossWrapper.py
@@ -8,16 +8,6 @@ class CTCLossWrapper(CTCLoss):
                 **batch) -> Tensor:
         log_probs_t = torch.transpose(log_probs, 0, 1)
 
-        # print(f"log_probs_length: {log_probs_length}")
-        # print(f"text_encoded_length: {text_encoded_length}")
-
-        # print(
-        #     f'log_probs : {log_probs_t.size()}\n'
-        #     f'targets : {text_encoded.size()}\n'
-        #     f'input_lengths : {log_probs_length.size()}\n'
-        #     f'target_lengths : {text_encoded_length.size()}\n'
-        # )
-
         return super().forward(
             log_probs=log_probs_t,
             targets=text_encoded,
